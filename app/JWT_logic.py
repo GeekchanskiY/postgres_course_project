1fc5642095c6acf6eb5f2abe488de3cc230f4e50
GeekchanskiY <astrovskiy.04@gmail.com>
2023-05-10 08:05:47 +0300

    stage

--- app/JWT_logic.py
+++ app/JWT_logic.py
@@ -1,3 +1,52 @@
-from jwt import decode
+''' JWT encode, decode and storing '''
 
-decode('oKIL1lQ2TWqm6gypN45OVjhkqw8CBPDfqoA3lkuY3W4=', 'your-secret-key', algorithms=['HS256'])
+import jwt
+
+
+class JWTHolder:
+    current_token: str = None
+    algorithm = 'HS256'
+    secret = 'secret'
+    username: str
+    uid: int
+    role: str
+
+    def __init__(self, uid, username, role, algorithm=None, secret=None):
+        self.username = username
+        self.uid = uid
+        self.role = role
+
+        if algorithm is not None:
+            self.algorithm = algorithm
+        if secret is not None:
+            self.secret = secret
+
+    def create_jwt(self):
+        self.current_token = jwt.encode(
+            {
+                'uid': self.uid,
+                'username': self.username,
+                'role': self.role
+            },
+            self.secret,
+            algorithm=self.algorithm
+        )
+
+    def decode_jwt(self) -> str:
+        try:
+            return str(
+                jwt.decode(
+                    self.current_token,
+                    self.secret,
+                    algorithms=[self.algorithm]
+                )
+            )
+        except jwt.InvalidTokenError:
+            return 'err'
+
+
+if __name__ == "__main__":
+    holder = JWTHolder('1', 'admin', 'superuser')
+    holder.create_jwt()
+    print(holder.current_token)
+    print(holder.decode_jwt())
---
+++ app/connector.py
@@ -0,0 +1,30 @@
+''' PGSQL connection logic here '''
+
+from sqlalchemy import create_engine, text
+import psycopg2
+
+class CustomAdminConnector:
+    def __init__(self):
+        self.conn = psycopg2.connect(
+            host='0.0.0.0',
+            database='postgres',
+            user='postgres',
+            password='postgres'
+        )
+        self.engine = create_engine('postgresql+psycopg2://postgres:postgres@0.0.0.0/postgres')
+        self.cur = self.conn.cursor()
+
+    def exec(self, query_str: str) -> str:
+        self.cur.execute(query_str)
+        res = self.cur.fetchall()
+        with self.engine.connect() as conn:
+            result = conn.execute(text(query_str))
+            for row in result:
+                print(row)
+        return res
+
+
+if __name__ == '__main__':
+    cc = CustomAdminConnector()
+    print(cc.exec('SELECT * FROM USERS'))
+
